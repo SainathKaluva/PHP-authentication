bf8f9999ffb05ee1d505a8be33ddaf8ed82c5acf
llsaill <23387274+llsaill@users.noreply.github.com>
2018-01-17 01:10:46 +0100

    Created a database for storing accounts

---
+++ int/config.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST", "localhost");   
+define("USER", "marton_sysadmin");    
+define("PASSWORD", "ys!deFrRDHN"); 
+define("DATABASE", "marton");
+
+$mysqli = new mysqli (HOST, USER, PASSWORD, DATABASE); //Connects the application to database
---
+++ profile.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Marton Intl.</title>
+	</head>
+	<body>
+		<b>Welcome : You are logged in </b>
+		<b><a href="index.php">Log Out</a></b>
+	</body>
+</html>
